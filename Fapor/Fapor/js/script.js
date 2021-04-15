e66c7db3ba4b551241f3cf8c50949b1d08cc2ce8
AlexHab <sam_climate@mail.ru>
2021-04-15 21:07:51 +1000

    15.04.2021

--- Fapor/Fapor/js/script.js
+++ Fapor/Fapor/js/script.js
@@ -17,29 +17,30 @@ var mySwiper = new Swiper('.swiper-container', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
- 
+
 	// If we need pagination
 	pagination: {
-	  el: '.swiper-pagination',
+		el: '.swiper-pagination',
+		clickable: true,
 	},
- 
+
 	// Navigation arrows
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
 	},
- 
+
 	// And if we need scrollbar
 	/*scrollbar: {
 	  el: '.swiper-scrollbar',
 	},*/
- });
+});
 
 //========================Бургер=================================
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		 $('.header__burger,.header__menu').toggleClass('active');
-		 $('body').toggleClass('lock');
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
 });
 
@@ -47,20 +48,20 @@ $(document).ready(function() {
 //  alert('Hello Gulp');
 ;
 function testWebP(callback) {
-  var webP = new Image();
-  webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-  };
-  webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src =
+		"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
-  if (support == true) {
-    document.querySelector("body").classList.add("webp");
-  } else {
-    document.querySelector("body").classList.add("no-webp");
-  }
+	if (support == true) {
+		document.querySelector("body").classList.add("webp");
+	} else {
+		document.querySelector("body").classList.add("no-webp");
+	}
 });
 
 
