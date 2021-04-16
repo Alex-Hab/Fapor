2bac16c364b93f18397823ca8e0fc055c8f353df
Alex-Hab <sam_climate@mail.ru>
2021-04-16 17:59:25 +1000

    16/04/2021

--- Fapor/#src/js/script.js
+++ Fapor/#src/js/script.js
@@ -1,6 +1,6 @@
 //=====================Подключение SWIPER========================
 @@include("swiper-bundle.min.js");
-var mySwiper = new Swiper('.swiper-container', {
+/*var mySwiper = new Swiper('.swiper-container', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
@@ -21,7 +21,93 @@ var mySwiper = new Swiper('.swiper-container', {
 	/*scrollbar: {
 	  el: '.swiper-scrollbar',
 	},*/
-});
+//});
+let sliders = document.querySelectorAll('.swiper-wrapper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+		let slider_content = slider.innerHTML;
+		let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');				
+	}
+	sliders_bild_callback();
+}
+function sliders_bild_callback(params) { }
+
+if (document.querySelector('.slider-header')) {
+	let mainslider = new Swiper('.slider-header-wrapper', {
+		// Optional parameters
+		//direction: 'horizontal',
+		loop: true,
+		observer: true,
+		observeParents: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		speed: 800,
+
+		// If we need pagination
+		pagination: {
+			el: '.slider-header__pagination',			
+			clickable: true,
+		},
+		on: {
+			lazyImageReady: function () {
+				ibg();
+			},
+		},
+
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},		
+	});			
+}
+if (document.querySelector('.slider-feedback')) {
+	let mainslider = new Swiper('.slider-feedback__wrapper', {
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+	});
+}
+if (document.querySelector('.brands-slider')) {
+	let brandsSlider = new Swiper('.brands-slider__wrapper', {
+		slidesPerView: 5,
+		spaceBetween: 0,		
+		speed: 800,
+		loop: true,		
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+				autoHeight: true,
+			},
+			480: {
+				slidesPerView: 2,
+			},
+			600: {
+				slidesPerView: 3,
+			},
+			768: {
+				slidesPerView: 4,
+			},
+			992: {
+				slidesPerView: 5,
+			},
+		}
+	});
+}
+
 
 //========================Бургер=================================
 $(document).ready(function () {
